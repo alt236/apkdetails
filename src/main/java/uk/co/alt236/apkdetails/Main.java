8ed72963702dcd80da8cf6a6c29d6c65797ec8f9
Alexandros Schillings <aschillings@gmail.com>
2017-11-21 15:19:34 +0000

    KV Printer is no longer an inner class

--- src/main/java/uk/co/alt236/apkdetails/Main.java
+++ src/main/java/uk/co/alt236/apkdetails/Main.java
@@ -3,10 +3,7 @@ package uk.co.alt236.apkdetails;
 import uk.co.alt236.apkdetails.decoder.ManifestParser;
 import uk.co.alt236.apkdetails.model.Manifest;
 
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 
 public class Main {
 
@@ -43,44 +40,4 @@ public class Main {
     }
 
 
-    private static class KVPrinter {
-        private final List<String> keys;
-        private final List<String> values;
-
-        public KVPrinter() {
-            keys = new ArrayList<>();
-            values = new ArrayList<>();
-        }
-
-        public void add(String key, long value) {
-            keys.add(key);
-            values.add(String.valueOf(value));
-        }
-
-        public void add(String key, boolean value) {
-            keys.add(key);
-            values.add(String.valueOf(value));
-        }
-
-        public void add(String key, String value) {
-            keys.add(key);
-            values.add(value);
-        }
-
-        public void print() {
-            final Optional<String> maxString = keys.stream().max(Comparator.comparingInt(String::length));
-            final int maxStringSize = maxString.get().length();
-
-            for (int i = 0; i < keys.size(); i++) {
-                final String key = keys.get(i);
-                final String value = values.get(i);
-
-                System.out.println(padRight(key, maxStringSize) + ": " + value);
-            }
-        }
-
-        private static String padRight(String s, int n) {
-            return String.format("%1$-" + n + "s", s);
-        }
-    }
 }
