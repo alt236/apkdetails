167a4b4ca2ac6a0152d8f42a1bcb775c91d6175e
Alexandros Schillings <aschillings@gmail.com>
2017-11-28 15:24:48 +0000

    Now listing number of inner classes

--- src/main/java/uk/co/alt236/apkdetails/output/DexInfoOutput.java
+++ src/main/java/uk/co/alt236/apkdetails/output/DexInfoOutput.java
@@ -1,8 +1,8 @@
 package uk.co.alt236.apkdetails.output;
 
 import uk.co.alt236.apkdetails.print.section.OutputCollector;
-import uk.co.alt236.apkdetails.repo.DexRepository;
 import uk.co.alt236.apkdetails.repo.common.ZipContents;
+import uk.co.alt236.apkdetails.repo.dex.DexRepository;
 
 public class DexInfoOutput implements Output {
     private final ZipContents zipContents;
@@ -14,11 +14,13 @@ public class DexInfoOutput implements Output {
     @Override
     public void output(OutputCollector printer) {
         final DexRepository dexContents = new DexRepository(zipContents);
-
+        final long dexClasses = dexContents.getTotalClassCount();
+        final long anonClasses = dexContents.getAnonymousClassCount();
         printer.add("Dex Contents");
         printer.startKeyValueSection();
         printer.addKv("Dex Files", dexContents.getDexFiles().size());
-        printer.addKv("Dex Classes", dexContents.getTotalClasses());
+        printer.addKv("Dex Classes", dexClasses);
+        printer.addKv("Dex Anon Classes", anonClasses + "/" + dexClasses);
         printer.addKv("Dex Methods", dexContents.getTotalMethods());
         printer.addKv("Dex Strings", dexContents.getTotalStrings());
         printer.endKeyValueSection();
