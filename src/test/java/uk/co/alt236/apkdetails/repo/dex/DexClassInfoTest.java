7050260bb93c9e0542de4cbd6fac19c493f43220
Alexandros Schillings <aschillings@gmail.com>
2017-12-04 16:43:21 +0000

    Added lambda count

--- src/test/java/uk/co/alt236/apkdetails/repo/dex/DexClassInfoTest.java
+++ src/test/java/uk/co/alt236/apkdetails/repo/dex/DexClassInfoTest.java
@@ -19,23 +19,65 @@ public class DexClassInfoTest {
     }
 
     @Test
-    public void shouldNotMatchOuterClass() {
+    public void isInnerClassShouldNotMatchOuterClass() {
         final ClassDef clazz = Mockito.mock(ClassDef.class);
         when(clazz.getType()).thenReturn("Landroid/support/v4/app/FragmentTransition;");
         assertFalse(dexClassInfo.isInnerClass(clazz));
     }
 
     @Test
-    public void shouldNotMatchOuterClassEndingWithNumber() {
+    public void isInnerClassShouldNotMatchOuterClassEndingWithNumber() {
         final ClassDef clazz = Mockito.mock(ClassDef.class);
         when(clazz.getType()).thenReturn("Landroid/support/v4/app/FragmentTransition1;");
         assertFalse(dexClassInfo.isInnerClass(clazz));
     }
 
     @Test
-    public void shouldMatchInnerClass() {
+    public void isInnerClassShouldMatchInnerClass() {
         final ClassDef clazz = Mockito.mock(ClassDef.class);
         when(clazz.getType()).thenReturn("Landroid/support/v4/app/FrameMetricsAggregator$FrameMetricsApi24Impl$1;");
         assertTrue(dexClassInfo.isInnerClass(clazz));
+    }
+
+    @Test
+    public void isLambdaShouldNotMatchOuterClass() {
+        final ClassDef clazz = Mockito.mock(ClassDef.class);
+        when(clazz.getType()).thenReturn("Landroid/support/v4/app/FragmentTransition;");
+        assertFalse(dexClassInfo.isLambda(clazz));
+    }
+
+    @Test
+    public void isLambdaShouldNotMatchOuterClassEndingWithNumber() {
+        final ClassDef clazz = Mockito.mock(ClassDef.class);
+        when(clazz.getType()).thenReturn("Landroid/support/v4/app/FragmentTransition1;");
+        assertFalse(dexClassInfo.isLambda(clazz));
+    }
+
+    @Test
+    public void isLambdaNotShouldMatchNonLambdaInnerClass() {
+        final ClassDef clazz = Mockito.mock(ClassDef.class);
+        when(clazz.getType()).thenReturn("Landroid/support/v4/app/FrameMetricsAggregator$FrameMetricsApi24Impl$1;");
+        assertFalse(dexClassInfo.isLambda(clazz));
+    }
+
+    @Test
+    public void isLambdaNotShouldMatchInnerClassCalledLambda() {
+        final ClassDef clazz = Mockito.mock(ClassDef.class);
+        when(clazz.getType()).thenReturn("Landroid/support/v4/app/FrameMetricsAggregator$Lambda;");
+        assertFalse(dexClassInfo.isLambda(clazz));
+    }
+
+    @Test
+    public void isLambdaNotShouldMatchInnerClassCalledLambdaWithAnonymousClass() {
+        final ClassDef clazz = Mockito.mock(ClassDef.class);
+        when(clazz.getType()).thenReturn("Landroid/support/v4/app/FrameMetricsAggregator$Lambda$0;");
+        assertFalse(dexClassInfo.isLambda(clazz));
+    }
+
+    @Test
+    public void isLambdaShouldMatchLambdaInnerClass() {
+        final ClassDef clazz = Mockito.mock(ClassDef.class);
+        when(clazz.getType()).thenReturn("Landroid/support/v4/app/FrameMetricsAggregator$$Lambda$0;");
+        assertTrue(dexClassInfo.isLambda(clazz));
     }
 }
