6eb06cec050a2ab188a6c8e2123d07c386090a16
Alexandros Schillings <aschillings@gmail.com>
2019-02-08 11:38:52 +0000

    Added option to recursively search a directory for APKs

--- src/main/java/uk/co/alt236/apkdetails/ApkFileFilter.java
+++ src/main/java/uk/co/alt236/apkdetails/ApkFileFilter.java
@@ -1,35 +1,61 @@
 package uk.co.alt236.apkdetails;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Stream;
 
-public class ApkFileFilter {
+class ApkFileFilter {
 
-    public List<String> getFiles(final String source) {
+    List<String> getFiles(final String source,
+                          boolean recursive) {
         final File file = new File(source);
-        return getFiles(file);
+        return getFiles(file, recursive);
     }
 
-    public List<String> getFiles(final File file) {
+    List<String> getFiles(final File file,
+                          boolean recursive) {
         final List<String> retVal = new ArrayList<>();
 
         if (file.exists()) {
             if (file.isDirectory()) {
-                final File[] files = file.listFiles(
-                        (dir, name) -> name.toLowerCase(Locale.US).endsWith(".apk"));
-
-                if (files != null) {
-                    for (final File apk : files) {
-                        retVal.add(apk.getAbsolutePath());
-                    }
-                }
+                final int depth = recursive ? Integer.MAX_VALUE : 1;
+                retVal.addAll(getContainedFiles(file, depth));
             } else {
                 retVal.add(file.getAbsolutePath());
             }
         }
 
+        Collections.sort(retVal);
         return retVal;
+    }
+
+    private List<String> getContainedFiles(final File directory,
+                                           int depth) {
+        final List<String> retVal = new ArrayList<>();
+
+        try (Stream<Path> stream = Files.find(Paths.get(directory.toURI()),
+                depth,
+                this::isApk)) {
+
+            stream.forEach(path -> retVal.add(path.toAbsolutePath().toString()));
+        } catch (IOException e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        }
+
+        return retVal;
+    }
+
+    private boolean isApk(Path path,
+                          BasicFileAttributes fileAttr) {
+        return fileAttr.isRegularFile()
+                && path.toString().toLowerCase(Locale.US).endsWith(".apk");
     }
 }
